9c285ba4135b8197fad8f035fc289e20d9154662
Wouter Lievens <lievenswouter@gmail.com>
2016-04-18 21:59:37 +0200

    starting on intersection code

--- src/main/java/symgeom/geom/Cubic.java
+++ src/main/java/symgeom/geom/Cubic.java
@@ -2,8 +2,11 @@ package symgeom.geom;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import symgeom.value.Value;
 
 import java.util.List;
+
+import static symgeom.value.Value.number;
 
 @Getter
 @RequiredArgsConstructor
@@ -16,6 +19,17 @@ public class Cubic
 
     public List<Point> intersections(Segment segment)
     {
+        Value a = start.getX();
+        Value b = control1.getX();
+        Value c = control2.getX();
+        Value d = end.getX();
+
+        Value b_a = b.divide(a).simplify();
+        Value c_a = c.divide(a).simplify();
+        Value d_a = d.divide(a).simplify();
+
+        Value q = (number(3).multiply(c_a).subtract() ).divide(number(9));
+
         return null;
     }
 }
